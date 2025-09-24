c4db638e51b7c5ec5a399cb473e8cb6a0760a963
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2025-09-24 11:27:57 +0200

    Token ERC-20

---
+++ 01-NEARX/01-Foundry/03-tokenERC-20/src/Token.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+// If using Foundry, update to:
+import {ERC20} from "lib/solady/src/tokens/ERC20.sol";
+
+
+contract Token is ERC20 {
+    constructor(uint256 amount, string memory /* name */) {
+        _mint(msg.sender, amount * 1e18);
+    }
+
+    function name() public pure override returns (string memory) {
+        return "My Token";
+    }
+
+    function symbol() public pure override returns (string memory) {
+        return "TOKEN";
+    }
+
+    // (opcional) padronize 18 casas
+    // function decimals() public view override returns (uint8) { return 18; }
+}
